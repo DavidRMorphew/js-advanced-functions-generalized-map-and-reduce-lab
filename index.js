13be4e6a78137c8e2bca17dbe7b1de21c55ee4d6
DavidRMorphew <morphewd@umich.edu>
2021-04-08 14:51:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(initialArray, callBackFunction){
+    const newArray = [];
+    initialArray.forEach(element => newArray.push(callBackFunction(element)));
+    return newArray;
+};
+
+function reduce(initialArray, callBackFunction, initialValue = 0){
+    const nonNumberArray = initialArray.some(el => { return typeof el !== "number"})
+    let total;
+    if (!!nonNumberArray) {
+        total = true;
+    } else {
+        total = initialValue;
+    };
+    initialArray.forEach(element => {
+        total = callBackFunction(element, total)
+    });
+    return total;
+};
